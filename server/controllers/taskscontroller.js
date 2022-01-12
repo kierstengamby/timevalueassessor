db31ef44dddf0f54ce7338f4ad091e53ca3306a5
Kiersten Gamby <kierstengamby@gmail.com>
2022-01-12 16:11:18 -0500

    tasks controller updated and complete

--- server/controllers/taskscontroller.js
+++ server/controllers/taskscontroller.js
@@ -30,4 +30,68 @@ router.post('/tasks', async(req, res) => {
     };
 });
 
+router.put('/tasks', async(req, res) => {
+    const { cleaning, laundry, mealPrep, petCare, shopping, carCare, taxes } = req.body.tasks;
+    const tasksId = req.body.tasks.id;
+
+    const query = {
+        where: {
+            id: tasksId
+        }
+    };
+
+    const updateTasksModel = { cleaning, laundry, mealPrep, petCare, shopping, carCare, taxes };
+
+    try{
+        const update = await models.TasksModel.update(updateTasksModel, query);
+        res.status(200).json({ message: 'Tasks post successfully edited' }); 
+    } catch(err) {
+        res.status(500).json({ error: `${err}` });
+    }
+});
+
+router.get('/tasks', async(req, res) => {   
+    const { userId } = req.user.id;
+
+    const getAll = {
+        where: {
+            id: userId
+        }
+    };
+
+    try {
+        const allTasksPosts = await models.TasksModel.findAll({ getAll })
+        res.status(200).json(allTasksPosts)
+    } catch(err) {
+        res.status(500).json({
+            error: `${err}`
+        })
+    }
+});
+
+router.delete('/tasks', async(req, res) =>{
+    try {
+        await models.TasksModel.destroy({ 
+            where: {
+                id: req.body.id 
+            }
+        }).then((result) => {
+            if(result) {
+                res.status(200).json({
+                    message: 'Tasks post successfully deleted',
+                    deletedResult: `${result}`
+                })
+            } else {
+                res.status(400).json({
+                    message: 'Tasks post does not exist'
+                })
+            }
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: `Failed to delete Tasks post: ${err}`
+        })
+    }
+});
+
 module.exports = router;
