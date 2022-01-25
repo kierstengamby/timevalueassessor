4bd724e5d50ad53f9afe4e541fb601b36cb840e8
Kiersten Gamby <kierstengamby@gmail.com>
2022-01-25 15:06:33 -0500

    update prior to heroku deployment

--- client/src/home/Splash.js
+++ client/src/home/Splash.js
@@ -1,60 +1,25 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import ValueCalcIndex from '../components/site/Values/ValueCalcIndex';
 import TimeIndex from '../components/site/Time/TimeIndex';
 import TasksIndex from '../components/site/Task/TasksIndex';
 import Stopwatch from '../components/site/Stopwatch/Stopwatch';
 import Homepage from '../components/site/Homepage/Homepage';
-import { Carousel, CarouselItem } from 'reactstrap';
 
-function Splash(props) {
-    const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex)
-    };
-
+const Splash = (props) => {
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect} arrows >
-            <CarouselItem>
-                <Homepage token={props.sessionToken} />
-            </CarouselItem>
-            <CarouselItem>
-                <ValueCalcIndex token={props.sessionToken} tasks={props.tasks} time={props.time} />
-            </CarouselItem>
-            <CarouselItem>
-                <TimeIndex token={props.sessionToken} time={props.time} />
-            </CarouselItem>
-            <CarouselItem>
-                <TasksIndex token={props.sessionToken} tasks={props.tasks} />
-            </CarouselItem>
-            <CarouselItem>
-                <Stopwatch token={props.sessionToken} />
-            </CarouselItem>
-        </Carousel>
-    );
-    };
-
-
-// const Splash = (props) => {
-//     return (
-//         <Carousel>
-//             <CarouselItem>
-//                 <Homepage token={props.sessionToken} />
-//                 <br /><br />
-//             </CarouselItem>
-//             <CarouselItem><ValueCalcIndex token={props.sessionToken} tasks={props.tasks} time={props.time} />
-//                 <br /><br /><br />
-//             </CarouselItem>
-//             <CarouselItem><TimeIndex token={props.sessionToken} time={props.time} />
-//                 <br /><br />
-//             </CarouselItem>
-//             <CarouselItem>
-//                 <TasksIndex token={props.sessionToken} tasks={props.tasks} />
-//                 <br />
-//             </CarouselItem><CarouselItem><Stopwatch token={props.sessionToken} />
-//             </CarouselItem>
-//         </Carousel>
-//     )
-// }
+        <div>
+            <Homepage token={props.sessionToken} />
+            <br /><br />
+            <ValueCalcIndex token={props.sessionToken} tasks={props.tasks} time={props.time} />
+            <br /><br /><br />
+            <TimeIndex token={props.sessionToken} time={props.time} />
+            <br /><br />
+            <TasksIndex token={props.sessionToken} tasks={props.tasks} />
+            <br />
+            <Stopwatch token={props.sessionToken} />
+        </div>
+    )
+}
 
 export default Splash;
