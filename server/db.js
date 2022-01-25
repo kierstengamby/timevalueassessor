327a8097c5f07fec6b9d6b6b8ced9846eef3694f
Kiersten Gamby <kierstengamby@gmail.com>
2022-01-25 16:10:11 -0500

    heroku deploy

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 // const seqeulize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
 //     host: process.env.DATABASE_HOST,
 //     dialect: process.env.DATABASE_DIALECT
 // })
 
-new Sequelize(
-    process.env.DATABASE_URL  || 
+const seqeulize = new Sequelize (
+    // process.env.DATABASE_URL  || 
     `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost:5432/timevalueassessor`,
     {
         dialect: 'postgres',
